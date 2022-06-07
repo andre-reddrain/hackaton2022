ff274eaf894352335d460f687851dc570ffd4589
André Rosa <andre.reddrain@gmail.com>
2022-06-07 18:40:32 +0100

    prev commit.

--- pages/api/resources/getResources.js
+++ pages/api/resources/getResources.js
@@ -1,3 +1,10 @@
-export default function handler(req, res) {
-    res.status(200).json({ name: 'Get Resources!' })
+import { getResources } from "../../../services/resources/getResources"
+
+export default async function getAllResources(req, res) {
+    if (req.method === "GET") {
+        let ans = await getResources()
+        console.log(ans)
+        // let ans = { name: "Get all cats!" }
+        return res.status(200).json({ ans })
+    }
 }
