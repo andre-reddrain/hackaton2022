b85169e80bc2e1718688f0996765475826fb9a31
André Rosa <andre.reddrain@gmail.com>
2022-06-07 17:11:06 +0100

    Delete Cat.

--- pages/api/cats/deleteCat.js
+++ pages/api/cats/deleteCat.js
@@ -1,3 +1,10 @@
-export default function handler(req, res) {
-    res.status(200).json({ name: 'Delete one cat!' })
+import { deleteCat } from "../../../services/cats/deleteCat"
+
+export default async function deleteOneCat(req, res) {
+    if (req.method === "DELETE") {
+        //TODO REMOVE TEST
+        let ans = await deleteCat("629f6e6429b0e642f3468227")
+        console.log(ans)
+        return res.status(200).json({ ans })
+    }
 }
