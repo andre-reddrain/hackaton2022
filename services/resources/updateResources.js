6294c93b38a1641dd404baa84a704dc9601930fa
André Rosa <andre.reddrain@gmail.com>
2022-06-08 04:38:18 +0100

    Todo

--- services/resources/updateResources.js
+++ services/resources/updateResources.js
@@ -1,5 +1,6 @@
 import { findAllResources } from "../../data/resources";
 
+// TODO Finish UpdateResources: Data / Services / API; Request
 export async function updateResources() {
     return await findAllResources()
 }
