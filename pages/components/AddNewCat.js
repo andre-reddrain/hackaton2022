6addf52544cbce784bc7961fed002ca029df1f25
André Rosa <andre.reddrain@gmail.com>
2022-06-08 10:42:04 +0100

    Fix on button

--- pages/components/AddNewCat.js
+++ pages/components/AddNewCat.js
@@ -1,108 +1,109 @@
 // import styles from '../styles/Home.module.css'
-import {useState} from "react"
+import { useState } from "react"
 import styles from '../../styles/Forms.module.css'
 
 export default function AddNewCat(props) {
-    const [catInfo, setCatInfo] = useState({
-        Name: "",
-        Age: 0,
-        Breed: "",
-        Weight: 0
+  const [catInfo, setCatInfo] = useState({
+    Name: "",
+    Age: 0,
+    Breed: "",
+    Weight: 0
+  })
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    //TODO AUTOMATIC REFRESH OF DASHBOARD STATE
+    createCat(catInfo.Name, catInfo.Age, catInfo.Breed, catInfo.Weight)
+
+    props.setViewCatForm(false)
+  }
+
+  async function createCat(name, age, breed, weight) {
+    const costFood = 1.4
+    const costSand = 1.5
+
+    const isBaby = ((age) => {
+      if (age <= 1) return true
+      return false
     })
 
-    function handleSubmit(e) {
-      e.preventDefault()
+    const setCosts = ((isBaby) => {
+      if (isBaby) return [costFood / 2, costSand / 2]
+      return [costFood, costSand]
+    })
 
-      //TODO AUTOMATIC REFRESH OF DASHBOARD STATE
-      createCat(catInfo.Name, catInfo.Age, catInfo.Breed, catInfo.Weight)
-
-      props.setViewCatForm(false)
-    }
-
-    async function createCat(name, age, breed, weight) {
-      const costFood = 1.4
-      const costSand = 1.5
-
-      const isBaby = ((age) => {
-        if (age <= 1) return true
-        return false
+    const res = await fetch(`../../api/cats/addCat`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        age: age,
+        breed: breed,
+        weight: weight,
+        dewormer: new Date(),
+        costs: setCosts(isBaby(age))
       })
-
-      const setCosts = ((isBaby) => {
-        if (isBaby) return [costFood / 2, costSand / 2]
-        return [costFood, costSand]
-      })
-
-      const res = await fetch(`../../api/cats/addCat`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          name: name,
-          age: age,
-          breed: breed,
-          weight: weight,
-          dewormer: new Date(),
-          costs: setCosts(isBaby(age))
-        })
-      })
-      const json = await res.json()
-    }
+    })
+    const json = await res.json()
+  }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <fieldset
-        className={styles.resourcesForm}>
+          className={styles.resourcesForm}>
           <label>Name</label>
-          <input 
-            type="text" 
-            name="name" 
+          <input
+            type="text"
+            name="name"
             placeholder="eg. Malaquias"
             required
             onChange={(e) => setCatInfo({ ...catInfo, Name: e.target.value })}>
-          </input><br/>
+          </input><br />
 
           <label>Age</label>
-          <input 
+          <input
             type="number"
             name="age"
             min="0"
-            placeholder="eg. 4" 
+            placeholder="eg. 4"
             required
             onChange={(e) => setCatInfo({ ...catInfo, Age: e.target.value })}>
-          </input><br/>
+          </input><br />
 
           <label>Race</label>
-          <input 
-            type="text" 
-            name="race" 
+          <input
+            type="text"
+            name="race"
             required
             placeholder="eg. Maine Coon"
             onChange={(e) => setCatInfo({ ...catInfo, Race: e.target.value })}>
-          </input><br/>
+          </input><br />
 
           <label>Weight</label>
-          <input 
-            type="text" 
-            name="weight" 
+          <input
+            type="text"
+            name="weight"
             required
             placeholder="eg. 6kg"
             onChange={(e) => setCatInfo({ ...catInfo, Weight: e.target.value })}>
           </input><br />
-<span>
-
-   <input 
-   className = {styles.buttonSubmit}
-   type="submit"></input>
-          <span>            
-            <button 
-            className = {styles.buttonClose}
-            onClick={() => props.setViewCatForm(e => !e)}>Close</button>
+          <span>
+            <input
+              className={styles.buttonSubmit}
+              type="submit"
+              value="Submit">
+            </input>
+            <span>
+              <button
+                className={styles.buttonClose}
+                onClick={() => props.setViewCatForm(e => !e)}>Close</button>
             </span>
-</span>
-         
+          </span>
+
         </fieldset>
       </form>
     </div>
